74eb44e0eec09863315f2068c7b280259f459a36
Hector Valenzuela <francisco1495@gmail.com>
2023-03-04 15:22:19 -0700

    Agregamos JS al clon, creamos tambien un nuevo HTML el cual es una lista del boton de tres lineas que esta en el header, todavia estamos en proceso de hacer la pagina dinamica

---
+++ script.js
@@ -0,0 +1,7 @@
+var threelinesbtn = document.querySelector('.threelines-menu')
+var inactive = document.getElementsByClassName('inactive')
+var threelinesMenuList = document.getElementsByClassName('threelines-menu-list')
+
+ function threelinesbtnclick(){
+    threelinesMenuList.classList.toggle('inactive')
+};
