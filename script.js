139079663f55eae413c42944188d4dc44dde4e67
Hector Valenzuela <francisco1495@gmail.com>
2023-03-11 17:58:03 -0700

    cambiamos algunas clases, tratamos de usar querryselectorAll pero no tuvimos exito, seguiremos intentando

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-var threeLinesBtn = document.querySelector('.threelines-menu');
-var navBarMenu = document.querySelector('nav-bar-menu');
-var inactive = document.querySelector('.inactive');
-var navLinkItem = document.querySelector('.nav-link-item');
-var inactiveChild = document.querySelector('.inactive-child');
+var threelinesbtn = document.querySelector('.threelines-menu')
+var inactive = document.querySelector('.inactive')
+var navBarMenu = document.querySelector('.nav-bar-menu')
+var navLinkItem = document.querySelectorAll('.nav-link-item')
+var navLinkItemArray = Array.from(navLinkItem)
+var navLinkItemChild = document.querySelectorAll('.nav-link-itemChild')
+var inactiveChild = document.querySelectorAll('.inactive-child')
 
-threeLinesBtn.addEventListener('click', toggleNavBarMenu )
-function toggleNavBarMenu(){
+
+threelinesbtn.addEventListener('click', threelinesbtnclick)
+function threelinesbtnclick(){
     navBarMenu.classList.toggle('inactive')
+};
+
+/*navLinkItem[0].addEventListener('click', navLinkItemClick)
+function navLinkItemClick(){
+    navLinkItemChild[0].classList.toggle('inactive-child')
 }
+
+navLinkItem[1].addEventListener('click', navLinkItemClick)
+function navLinkItemClick(){
+    navLinkItemChild[1].classList.toggle('inactive-child')
+}*/
+
+navLinkItem.forEach.addEventListener('click', navLinkItemClick)
+    function navLinkItemClick(){
+        navLinkItemChild.forEach.toggle(inactiveChild.forEach)
+    };
